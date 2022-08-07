2ab2bfab5a5347005fbf94c0172ea2016c0d3530
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-07 19:07:52 +0530

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-// const API_BASE_URL = "http://localhost:3000";
-// const API_BASE_URL1 = "http://localhost:3000/home";
+
 
 class UserService {
 
-//     homeview(){
-//         return axios.get(API_BASE_URL1);
-//     }
 }
 
 export default new UserService()
